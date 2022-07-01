0e331872a2842a4b89e5707a4ab3922d76c7d5c2
CjGalicia <carljeremygalicia@gmail.com>
2022-07-01 21:51:50 +0800

    Camera is now working

--- Contact-Tracing-VBversion2/QRCC.Designer.vb
+++ Contact-Tracing-VBversion2/QRCC.Designer.vb
@@ -22,9 +22,57 @@ Partial Class QRCC
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ScanBtn = New System.Windows.Forms.Button()
+        Me.QRPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Title = New System.Windows.Forms.Label()
+        CType(Me.QRPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ScanBtn
+        '
+        Me.ScanBtn.Location = New System.Drawing.Point(162, 323)
+        Me.ScanBtn.Name = "ScanBtn"
+        Me.ScanBtn.Size = New System.Drawing.Size(168, 44)
+        Me.ScanBtn.TabIndex = 0
+        Me.ScanBtn.Text = "Click to Scan"
+        Me.ScanBtn.UseVisualStyleBackColor = True
+        '
+        'QRPictureBox
+        '
+        Me.QRPictureBox.Location = New System.Drawing.Point(12, 56)
+        Me.QRPictureBox.Name = "QRPictureBox"
+        Me.QRPictureBox.Size = New System.Drawing.Size(449, 261)
+        Me.QRPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.QRPictureBox.TabIndex = 1
+        Me.QRPictureBox.TabStop = False
+        '
+        'Title
+        '
+        Me.Title.AutoSize = True
+        Me.Title.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Title.Location = New System.Drawing.Point(12, 9)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(449, 25)
+        Me.Title.TabIndex = 27
+        Me.Title.Text = "Quezon City Contact Tracing QR Scanner"
+        '
+        'QRCC
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(475, 382)
+        Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.QRPictureBox)
+        Me.Controls.Add(Me.ScanBtn)
+        Me.Name = "QRCC"
         Me.Text = "QRCC"
+        CType(Me.QRPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ScanBtn As Button
+    Friend WithEvents QRPictureBox As PictureBox
+    Friend WithEvents Title As Label
 End Class
--- Contact-Tracing-VBversion2/QRCC.vb
+++ Contact-Tracing-VBversion2/QRCC.vb
@@ -1,3 +1,23 @@
-﻿Public Class QRCC
+﻿Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
 
+
+Public Class QRCC
+    Dim Cam As VideoCaptureDevice
+    Dim BeatEmUp As Bitmap
+
+    Private Sub ScanBtn_Click(sender As Object, e As EventArgs) Handles ScanBtn.Click
+        Dim Era As VideoCaptureDeviceForm = New VideoCaptureDeviceForm
+        If Era.ShowDialog() = DialogResult.OK Then
+            Cam = Era.VideoDevice
+            AddHandler Cam.NewFrame, New NewFrameEventHandler(AddressOf PicBox)
+            Cam.Start()
+        End If
+    End Sub
+
+    Private Sub PicBox(sender As Object, eventArgs As NewFrameEventArgs)
+        BeatEmUp = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+        QRPictureBox.Image = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+    End Sub
 End Class
